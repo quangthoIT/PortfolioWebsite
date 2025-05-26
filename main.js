ac328b170d9cbf1398be5da1db223aa8acedf13e
quangthoit04 <quangthoit04@gmail.com>
2025-05-26 13:36:22 +0700

    Media Queries S6

--- main.js
+++ main.js
@@ -42,3 +42,11 @@ form.addEventListener("submit", (e) => {
       console.error("Error!", error.message);
     });
 });
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-150px";
+}
