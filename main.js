4d267124690f3b986b6204ef0f69d999957185e3
quangthoit04 <quangthoit04@gmail.com>
2025-05-26 13:00:27 +0700

    About Me Page S2

--- main.js
+++ main.js
@@ -4,3 +4,16 @@ var typed = new Typed(".full-name", {
   backSpeed: 150,
   loop: true,
 });
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(event, tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
